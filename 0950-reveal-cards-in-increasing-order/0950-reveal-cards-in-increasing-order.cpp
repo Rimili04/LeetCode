528ca45021ee5339952ddcf733899be533150ee7
RIMILI DUTTA <rimilidutta840@gmail.com>
2026-07-24 17:26:32 +0530

    Time: 0 ms (100%), Space: 12.7 MB (52.51%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(), deck.end());
+
+        queue<int> q;
+        int n = deck.size();
+
+        for (int i = 0; i < n; i++)
+            q.push(i);
+
+        vector<int> ans(n);
+
+        for (int x : deck) {
+            int idx = q.front();
+            q.pop();
+            ans[idx] = x;
+
+            if (!q.empty()) {
+                q.push(q.front());
+                q.pop();
+            }
+        }
+
+        return ans;
+    }
+};
