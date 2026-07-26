ce6400b8b40eec952d9eb662f6f7625aea76cb4f
RIMILI DUTTA <rimilidutta840@gmail.com>
2026-07-26 13:44:51 +0530

    Time: 0 ms (100%), Space: 10 MB (65.69%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,29 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {}
+
+    void push(int x) {
+        q.push(x);
+        int sz = q.size();
+        while (--sz) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
